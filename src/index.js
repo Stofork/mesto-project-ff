da9e1f5bda2d16ab804d67c7c2eac70b51c029c5
Stepanov Sergeu <sergeu.stepanov@yandex.ru>
2024-06-11 16:50:04 +0300

    Начало проекта

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import './pages/index.css'; // добавьте импорт главного файла стилей 
+import { initialCards } from './scripts/cards.js';
+import { createCard, deleteCard, likeCard } from './scripts/card.js';
 
-const numbers = [2, 3, 5];
+const cardList = document.querySelector('.places__list');
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
+initialCards.forEach(element => {
+    const card = createCard(element, deleteCard, likeCard);
+    cardList.append(card);
+});
 
-console.log(doubledNumbers); // 4, 6, 10 
+// Редактор профиля
+function pop() {
+    const data = document.querySelector('.popup_type_edit');
+    data.classList.add('popup_is-opened');
+    console.log(data);
+}
+// console.log(document.querySelector('.profile__edit-button'));
+const profileButton = document.querySelector('.profile__edit-button');
+profileButton.addEventListener('click', () => pop());
+
+// Добавление карточки
+
+function popCart() {
+    const data = document.querySelector('.popup__image');
+    data.classList.add('popup_is-opened');
+    console.log(data);
+}
+
+const profileButtonCard = document.querySelector('.profile__add-button');
+profileButtonCard.addEventListener('click', () => popCart());
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,15 +1,19 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+// const cardTemplate = document.querySelector('#card-template').content; -> f.createCard
 
 // @todo: DOM узлы
-const cardList = document.querySelector('.places__list');
+// const cardList = document.querySelector('.places__list'); -> index
 
 // @todo: Функция создания карточки
-function addCard(cardInfo, deleteCard) {
+function createCard(cardInfo, deleteCard, likeCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteButtonCard = cardElement.querySelector('.card__delete-button');
-
     const cardImage = cardElement.querySelector('.card__image');
+    
+    const likeButton = cardElement.querySelector('.card__like-button');
+    likeButton.addEventListener('click', likeCard);
+
     cardImage.src = cardInfo.link;
     cardImage.alt = 'Фотография места, ' + cardInfo.name;
 
@@ -26,8 +30,12 @@ function deleteCard(cardElement) {
     cardElement.remove();
 }
 
+const likeCard = (evt) => evt.target.classList.toggle('card__like-button_is-active');
+
 // @todo: Вывести карточки на страницу
-initialCards.forEach(element => {
-    const card = addCard(element, deleteCard);
-    cardList.append(card);
-});
+// initialCards.forEach(element => {
+//     const card = addCard(element, deleteCard);
+//     cardList.append(card);
+// }); -> index
+
+export { createCard, deleteCard, likeCard};
